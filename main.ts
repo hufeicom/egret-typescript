9d954bc0d622f8bc2aa092cb42a549c58b27476a
wildfirecode <wildfirecode13@gmail.com>
2018-10-28 11:59:08 +0800

    update

--- main.ts
+++ main.ts
@@ -15,14 +15,4 @@ class Main extends egret.DisplayObjectContainer {
 
 window['Main'] = Main;
 
-egret.runEgret({
-    renderMode: "webgl", audioType: 0, calculateCanvasScaleFactor: function (context: any) {
-        var backingStore = context.backingStorePixelRatio ||
-            context.webkitBackingStorePixelRatio ||
-            context.mozBackingStorePixelRatio ||
-            context.msBackingStorePixelRatio ||
-            context.oBackingStorePixelRatio ||
-            context.backingStorePixelRatio || 1;
-        return (window.devicePixelRatio || 1) / backingStore;
-    }
-});
+egret.runEgret();
